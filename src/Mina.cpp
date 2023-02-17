c0c5d1977b349575736f7ef42397a1c3a226ae7e
Nor-s <s28118149@gmail.com>
2023-02-17 21:03:44 +0900

    Add src files and CMakeLists.txt

---
+++ src/Mina.cpp
@@ -0,0 +1,35 @@
+#include "Mina.h"
+
+namespace Mina
+{
+Mina::Mina(entt::registry& registry) : registry(registry)
+{
+}
+
+void Mina::init()
+{
+}
+
+void Mina::loop()
+{
+	while (true)
+	{
+		preUpdate();
+		render();
+		postUpdate();
+	}
+}
+
+void Mina::preUpdate()
+{
+}
+
+void Mina::render()
+{
+}
+
+void Mina::postUpdate()
+{
+}
+
+}	 // namespace Mina
