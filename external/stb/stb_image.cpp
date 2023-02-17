46cabd9cdee26cb081dff16a2d46ea003429b890
Nor-s <s28118149@gmail.com>
2023-02-17 20:48:08 +0900

    Add submodule assimp, glm, stb

---
+++ external/stb/stb_image.cpp
@@ -0,0 +1,6 @@
+// https://stackoverflow.com/questions/64058036/preventing-multiple-define-when-including-headers
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.h"
+#include "stb_image_write.h"
